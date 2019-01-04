b57d79382a641300aafc1820138a765cdb36a786
devinaanderson <daa7896@gmail.com>
2019-01-04 14:01:12 -0700

    POST functionality in progress

--- src/components/checkout-form.js
+++ src/components/checkout-form.js
@@ -4,14 +4,25 @@ import {CardElement, injectStripe} from 'react-stripe-elements';
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
-    // this.submit = this.submit.bind(this);
+    this.state = {complete: false};
+    this.submit = this.submit.bind(this);
   }
 
-//   async submit(ev) {
-//     // User clicked submit
-//   }
+  async submit(ev) {
+    let {token} = await this.props.stripe.createToken({name: "Name"});
+    let response = await fetch("/charge", {
+        method: "POST",
+        headers: {"Content-Type": "text/plain"},
+        body: token.id
+    });
+
+    if (response.ok) console.log("Purchase Complete!")
+    if (response.ok) this.setState({complete: true});
+}
 
   render() {
+    if (this.state.complete) return <h1>Purchase Successful</h1>
+
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
