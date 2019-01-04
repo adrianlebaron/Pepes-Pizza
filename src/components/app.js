ec705cb88e9a31e54a40559c804bb92f1be8c89c
devinaanderson <daa7896@gmail.com>
2019-01-04 10:01:58 -0700

    added credit card input forms

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Elements, StripeProvider} from 'react-stripe-elements';
+import CheckoutForm from './checkout-form';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-      </div>
+      <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+        <div className="example">
+          <h1>Pepes Pizza</h1>
+          <h3>React Stripe Project</h3>
+          <Elements>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </StripeProvider>
     );
   }
 }
+
+export default App;
